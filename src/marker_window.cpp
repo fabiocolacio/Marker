367bcb7076adecf2b15a24833fa50d11ceb03794
Fabio Colacio <fcolacio98@gmail.com>
2017-08-16 13:58:06 -0700

    Add webkitgtk3 as a dependency

--- src/marker_window.cpp
+++ src/marker_window.cpp
@@ -2,12 +2,25 @@
 
 using namespace Gtk;
 
-MarkerWindow::MarkerWindow()
+MarkerWindow::MarkerWindow() :
+    paned(ORIENTATION_HORIZONTAL)
 {
     set_default_size(500, 600);
-    set_title("Editing: Untitled");
+    set_titlebar(headerbar);
     
-    add(source_view);
+    headerbar.set_title("Editing: Untitled");
+    headerbar.set_show_close_button(true);
+    headerbar.pack_start(preview_button);
+    
+    preview_button.set_image_from_icon_name("view-dual-symbolic");
+    
+    web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    Gtk::Widget* web_view_widget = Glib::wrap(GTK_WIDGET(web_view));
+    
+    add(paned);
+    
+    paned.add1(source_view);
+    paned.add2(*web_view_widget);
     
     show_all_children();
 }
--- src/marker_window.hpp
+++ src/marker_window.hpp
@@ -2,13 +2,21 @@
 #define MARKER_WINDOW_HPP
 
 #include <gtkmm/window.h>
+#include <gtkmm/button.h>
+#include <gtkmm/headerbar.h>
+#include <gtkmm/paned.h>
 #include <gtksourceviewmm/view.h>
+#include <webkit2/webkit2.h>
 
 using namespace Gtk;
 
 class MarkerWindow : public Window
 {
 private:
+    HeaderBar headerbar;
+    Button preview_button;
+    Paned paned;
+    WebKitWebView* web_view;
     Gsv::View source_view;
 public:
     MarkerWindow();
