134ccabc36da1133d61caec0f62e2233ca2bc6bd
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-01-13 15:38:02 -0800

    Minor tweak to the headers

--- src/marker-prefs.h
+++ src/marker-prefs.h
@@ -5,7 +5,7 @@
  *
  * Marker is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public License as
- * published by the Free Software Foundation; either version 2 of the
+ * published by the Free Software Foundation; either version 3 of the
  * License, or (at your option) any later version.
  *
  * Marker is distributed in the hope that it will be useful,
@@ -14,7 +14,7 @@
  * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
- * License along with the Gnome Library; see the file COPYING.LIB.  If not,
+ * License along with Marker; see the file LICENSE.md. If not,
  * see <http://www.gnu.org/licenses/>.
  *
  */
