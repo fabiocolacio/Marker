7403118237d521764cd08d72d71632f47196e0b9
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-01-13 00:24:25 -0800

    Add GPL headers

--- src/marker-utils.c
+++ src/marker-utils.c
@@ -1,3 +1,24 @@
+/*
+ * marker-utils.c
+ *
+ * Copyright (C) 2017 - 2018 Fabio Colacio
+ *
+ * Marker is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Marker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with the Gnome Library; see the file COPYING.LIB.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <time.h>
 #include <string.h>
 #include <stdio.h>
