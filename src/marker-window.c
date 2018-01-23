601f1a7a19fa3e167e3bf71d368279fa14652902
Martino Ferrari <manda.mgf@gmail.com>
2018-01-23 17:01:25 +0100

    removed unused methods

--- src/marker-window.c
+++ src/marker-window.c
@@ -337,34 +337,6 @@ key_pressed_cb (GtkWidget   *widget,
   return FALSE;
 }
 
-static void
-title_changed_cb (MarkerEditor *editor,
-                  const gchar  *title,
-                  gpointer      user_data)
-{
-  MarkerWindow *window = user_data;
-  gtk_header_bar_set_title (window->header_bar, title);
-}
-
-static void
-subtitle_changed_cb (MarkerEditor *editor,
-                     const gchar  *subtitle,
-                     gpointer      user_data)
-{
-  MarkerWindow *window = user_data;
-  gtk_header_bar_set_subtitle (window->header_bar, subtitle);
-}
-
-static void
-preview_zoom_changed_cb (MarkerPreview *preview,
-                         gpointer       user_data)
-{
-  MarkerWindow *window = user_data;
-  const gdouble zoom_percentage = 100 * webkit_web_view_get_zoom_level (WEBKIT_WEB_VIEW (preview));
-  g_autofree gchar *zoom_level_str = g_strdup_printf ("%.0f%%", zoom_percentage);
-  gtk_button_set_label (window->zoom_original_btn, zoom_level_str);
-}
-
 static gboolean
 window_deleted_event_cb (GtkWidget *widget,
                          GdkEvent  *event,
