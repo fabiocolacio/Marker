2c5c18b2287b4390b28c57a22adb2303e48d84f9
Martino Ferrari <manda.mgf@gmail.com>
2018-02-14 12:36:39 +0100

    fixed icon color problem

--- data/icons/hicolor/generate.sh
+++ data/icons/hicolor/generate.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-
-for n in $(ls *svg | sed 's/.svg//'); do inkscape -z -e $n.png -w $1 -h $1 $n.svg; done
+cd scalable/actions/
+for n in $(ls *svg | sed 's/.svg//'); do gtk-encode-symbolic-svg -o ../../$1/actions/ $n.svg $1; done
