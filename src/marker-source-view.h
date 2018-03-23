5b534ba455a4ffe320945d66efc8a9f89fdc5fda
Martino Ferrari <manda.mgf@gmail.com>
2018-03-24 00:29:04 +0100

    added insert_link function

--- src/marker-source-view.h
+++ src/marker-source-view.h
@@ -44,6 +44,7 @@ void                     marker_source_view_set_syntax_theme         (MarkerSour
                                                                       const char         *syntax_theme);
 void                     marker_source_view_surround_selection_with  (MarkerSourceView   *source_view,
                                                                       const char         *insertion);
+void                     marker_source_view_insert_link              (MarkerSourceView   *source_view);
 void                     marker_source_view_insert_image             (MarkerSourceView   *source_view,
                                                                       const char         *image_path);
 void                     marker_source_view_set_spell_check          (MarkerSourceView   *source_view,
