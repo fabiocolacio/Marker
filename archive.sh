912f8ec08ffad7dc0edcfbbbf104cdfa04a10bf0
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-04-27 12:20:34 -0700

    Create a script for creating release archives

---
+++ archive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/fabiocolacio/marker
+cd marker
+git submodule update --init --recursive
+rm -rf .git .gitignore .gitmodules
+cd ..
+zip -r marker.zip marker
