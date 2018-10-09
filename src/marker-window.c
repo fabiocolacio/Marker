0a3b35bd4d694bd528760f99887c6aec862220d3
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-10-08 19:42:33 -0700

    Use action accels rather than GDK key events

--- src/marker-window.c
+++ src/marker-window.c
@@ -347,40 +347,8 @@ key_pressed_cb (GtkWidget   *widget,
         marker_window_search(window);
         break;
 
-      case GDK_KEY_O:
-        marker_window_open_file_in_new_window(window);
-        break;
-
       case GDK_KEY_d:
         marker_window_open_sketcher (window);
-        break;
-
-      case GDK_KEY_S:
-        marker_window_save_active_file_as (window);
-        break;
-
-      case GDK_KEY_s:
-        marker_window_save_active_file (window);
-        break;
-
-      case GDK_KEY_p:
-        marker_preview_run_print_dialog (marker_editor_get_preview (editor), GTK_WINDOW (window));
-        break;
-
-      case GDK_KEY_1:
-        marker_editor_set_view_mode (editor, EDITOR_ONLY_MODE);
-        break;
-
-      case GDK_KEY_2:
-        marker_editor_set_view_mode (editor, PREVIEW_ONLY_MODE);
-        break;
-
-      case GDK_KEY_3:
-        marker_editor_set_view_mode (editor, DUAL_PANE_MODE);
-        break;
-
-      case GDK_KEY_4:
-        marker_editor_set_view_mode (editor, DUAL_WINDOW_MODE);
         break;
     }
   }
